b0ace90ed41c33085aed945826af71c856aca52f
Karan Shah <karanshah229@gmail.com>
2020-11-28 23:25:13 +0530

    Basic App

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var dbPromise, db;
+var dbPromise, db, users = [], currentUser, userNotes = [], userTasks = [], userLists = [];
 
 (function() {
 	'use strict';
@@ -21,6 +21,10 @@ var dbPromise, db;
 			var tasks_store = db.createObjectStore('tasks', { keyPath: 'id', autoIncrement: true });
 			tasks_store.createIndex('title', 'title', { unique: false });
 		}
+		if (!db.objectStoreNames.contains('lists')) {
+			var notes_store = db.createObjectStore('lists', { keyPath: 'id', autoIncrement: true });
+			notes_store.createIndex('title', 'title', { unique: false });
+		}
 		if (!db.objectStoreNames.contains('notes')) {
 			var notes_store = db.createObjectStore('notes', { keyPath: 'id', autoIncrement: true });
 			notes_store.createIndex('title', 'title', { unique: false });
@@ -29,6 +33,18 @@ var dbPromise, db;
 
 	dbPromise.onsuccess = function(event) {
 		db = event.target.result;
+
+		// addList(db, { userID: 'karan', listTitle: 'List1' });
+		// addList(db, { userID: 'karan', listTitle: 'List3' });
+		// addList(db, { userID: 'karan', listTitle: 'List2' });
+		// addUser(db, 'karan');
+		// addUser(db, 'karan1');
+		// addUser(db, 'karan12');
+
+		getUsers();
+		getUserLists();
+		// getUserTasks();
+		// getUserNotes();
 	}
 	dbPromise.onerror = function(event) {
 		alert("Data will not be stored as application. Check console for more details");
@@ -58,7 +74,7 @@ function addTask(db, data){
 		title: data.taskTitle,
 		description: data.taskDescription,
 		date: data.taskDate,
-		taskList: data.taskList || "default",
+		taskListID: data.taskListID || -1,
 		startTime: data.taskStarTime,
 		endTime: data.taskEndTime,
 		remindTime: data.taskRemindTime,
@@ -89,6 +105,77 @@ function addNote(db, data){
 	tx.onerror = function(event) {
 		alert("Couldn't create new Note. Check console for more details");
 		console.error('error storing note ' + event.target.errorCode);
+	}
+}
+
+function addList(db, data){
+	var tx = db.transaction('lists', 'readwrite');
+	var store = tx.objectStore('lists');
+	var list = {
+		userID: data.userID,
+		title: data.listTitle,
+		created: new Date().getTime()
+	};
+	store.add(list);
+	
+	tx.oncomplete = function() { console.log(`Added List to the Lists Store!`); }
+	tx.onerror = function(event) {
+		alert("Couldn't create new List. Check console for more details");
+		console.error('error storing list ' + event.target.errorCode);
+	}
+}
+
+function getUserLists(){
+	var tx = db.transaction('lists', 'readonly');
+	var store = tx.objectStore('lists');
+
+	var req = store.openCursor();
+
+	req.onsuccess = function(event){
+		let cursor = event.target.result;
+		if (cursor != null) {
+			if(cursor.value.userID == currentUser){
+				userLists.push(cursor.value);
+				var ele = document.createElement('div');
+				ele.classList.add('nav_item');
+				ele.style.paddingLeft = '2rem';
+				ele.textContent = cursor.value.title;
+				document.getElementById("user_lists").append(ele);
+			}
+			cursor.continue();
+		}
+	}
+	req.onerror = function(event){
+		alert("Couldn't fetch lists. Check console for more details");
+		console.error("error displaying lists " + event.target.errorCode);
+	}
+}
+
+function getUsers(){
+	var tx = db.transaction('users', 'readonly');
+	var store = tx.objectStore('users');
+
+	var req = store.openCursor();
+
+	req.onsuccess = function(event){
+		let cursor = event.target.result;
+		if (cursor != null) {
+			if(cursor.value.userID != currentUser){
+				console.log(cursor.value)
+				users.push(cursor.value);
+				var ele = document.createElement('li');
+				var i_ele = document.createElement('a');
+				i_ele.classList.add('black-text');
+				i_ele.textContent = cursor.value.name;
+				ele.append(i_ele);
+				document.getElementById("user_dropdown").prepend(ele);
+				cursor.continue();
+			}
+		}
+	}
+	req.onerror = function(event){
+		alert("Couldn't fetch lists. Check console for more details");
+		console.error("error displaying lists " + event.target.errorCode);
 	}
 }
 
